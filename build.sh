ad6dc9c68138fc03182e23a31e3b0173e0296a06
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2020-01-12 23:57:19 +0100

    org/cppmodel : reorg folders

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Compilation started";
-g++ -g -std=c++11 src/main.cpp src/model/model.cpp src/profile/profile.cpp src/model/model.h src/profile/profile.h -o build/model
+g++ -g -std=c++11 src/main.cpp \
+src/model/model.h src/model/model.cpp \
+src/profile/profile.h src/profile/profile.cpp \
+-o build/model
 echo "Compilation finished";
 echo "Runtime is ./build/model";
