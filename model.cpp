bbcf629e38c4cd72087937bb72bbed516b89d6cb
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2020-01-12 16:55:48 +0100

    init/cppmodel : init repo

---
+++ model.cpp
@@ -0,0 +1,483 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <chrono>
+#include <tuple>
+
+using namespace std;
+
+/**
+ * @brief item structure 
+ * 
+ */
+typedef struct
+{
+  int index;
+  int port;
+  string timestamp;
+  string type;
+  float value;
+} Item;
+
+/**
+ * @brief time profiler
+ * 
+ * @return double 
+ */
+double microtime()
+{
+  return (double(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count()) / double(1000000));
+}
+
+/**
+ * @brief Liste Class
+ * 
+ */
+class Liste
+{
+
+public:
+  enum Directions
+  {
+    ASC,
+    DESC
+  };
+  enum Views
+  {
+    MAIN,
+    FILTERED
+  };
+  Liste();
+  ~Liste();
+  vector<Item> items();
+  Item itemAt(int ix);
+  void resetAll();
+  bool removeAt(int ix);
+  void appendItem(Item item);
+  bool setItemAt(int ix, Item item);
+  void sortByIndex();
+  void sortByPort();
+  void sortByValue();
+  void sortByPortAndValue();
+  int getSize();
+  int getMinValue();
+  int getMinPort();
+  int getMinIndex();
+  int getMaxValue();
+  int getMaxPort();
+  int getMaxIndex();
+  void filterByPort(int portFilter);
+  void filterByValue(int valueFilter);
+  void displayFiltered();
+  void displayAt(int ix);
+  void displayAll();
+  void setOrder(Directions direction);
+  void setView(Views mode);
+
+private:
+  Directions order;
+  Views view;
+  vector<Item> itemList;
+  vector<Item> filteredList;
+};
+
+/**
+ * @brief Construct a new Temperature Liste:: Temperature Liste object
+ * 
+ */
+Liste::Liste()
+{
+  order = ASC;
+  resetAll();
+}
+
+/**
+ * @brief Destroy the Temperature Liste:: Temperature Liste object
+ * 
+ */
+Liste::~Liste()
+{
+}
+
+/**
+ * @brief set view mode as MAIN or FILTERED
+ * 
+ * @param mode 
+ */
+void Liste::setView(Views mode)
+{
+  view = mode;
+}
+
+/**
+ * @brief copy filtered items by port to the filtered list
+ * 
+ * @param portNumber 
+ */
+void Liste::filterByPort(int portFilter)
+{
+  filteredList = {};
+  std::copy_if(
+      itemList.begin(),
+      itemList.end(),
+      std::back_inserter(filteredList), [portFilter](const Item &i) { return i.port == portFilter; });
+}
+
+/**
+ * @brief copy filtered items by value to the filtered list
+ * 
+ * @param portNumber 
+ */
+void Liste::filterByValue(int valueFilter)
+{
+  filteredList = {};
+  std::copy_if(
+      itemList.begin(),
+      itemList.end(),
+      std::back_inserter(filteredList), [valueFilter](const Item &i) { return i.value == valueFilter; });
+}
+
+/**
+ * @brief reset lists
+ * 
+ */
+void Liste::resetAll()
+{
+  itemList = {};
+  filteredList = {};
+}
+
+/**
+ * @brief minimum value
+ * 
+ * @return int 
+ */
+int Liste::getMinValue()
+{
+  const auto min = std::min_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.value < i2.value;
+      });
+  return min->value;
+}
+
+/**
+ * @brief minimum port
+ * 
+ * @return int 
+ */
+int Liste::getMinPort()
+{
+  const auto min = std::min_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.port < i2.port;
+      });
+  return min->port;
+}
+
+/**
+ * @brief minimum index
+ * 
+ * @return int 
+ */
+int Liste::getMinIndex()
+{
+  const auto min = std::min_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.index < i2.index;
+      });
+  return min->index;
+}
+
+/**
+ * @brief maximum value
+ * 
+ * @return int 
+ */
+int Liste::getMaxValue()
+{
+  const auto max = std::max_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.value < i2.value;
+      });
+  return max->value;
+}
+
+/**
+ * @brief maximum port
+ * 
+ * @return int 
+ */
+int Liste::getMaxPort()
+{
+  const auto max = std::max_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.port < i2.port;
+      });
+  return max->port;
+}
+
+/**
+ * @brief maximum index
+ * 
+ * @return int 
+ */
+int Liste::getMaxIndex()
+{
+  const auto max = std::max_element(
+      itemList.begin(),
+      itemList.end(),
+      [](const Item &i1, const Item &i2) {
+        return i1.index < i2.index;
+      });
+  return max->index;
+}
+
+/**
+ * @brief sort list by index
+ * 
+ */
+void Liste::sortByIndex()
+{
+  const bool isAsc = (order == ASC);
+  std::sort(
+      itemList.begin(),
+      itemList.end(),
+      [isAsc](const Item &i1, const Item &i2) {
+        return (isAsc) ? (i1.index < i2.index) : (i1.index > i2.index);
+      });
+}
+
+/**
+ * @brief sort by port
+ * 
+ */
+void Liste::sortByPort()
+{
+  const bool isAsc = (order == ASC);
+  std::sort(
+      itemList.begin(),
+      itemList.end(),
+      [isAsc](const Item &i1, const Item &i2) {
+        return (isAsc) ? (i1.port < i2.port) : (i1.port > i2.port);
+      });
+}
+
+/**
+ * @brief sort by value
+ * 
+ */
+void Liste::sortByValue()
+{
+  const bool isAsc = (order == ASC);
+  std::sort(
+      itemList.begin(),
+      itemList.end(),
+      [isAsc](const Item &i1, const Item &i2) {
+        return (isAsc) ? (i1.value < i2.value) : (i1.value > i2.value);
+      });
+}
+
+/**
+ * @brief multicriterias sort by port and value
+ * 
+ */
+void Liste::sortByPortAndValue()
+{
+  const bool isAsc = (order == ASC);
+  std::sort(
+      itemList.begin(),
+      itemList.end(),
+      [isAsc](const Item &i1, const Item &i2) {
+        return (isAsc)
+                   ? std::tie(i1.port, i1.value) < std::tie(i2.port, i2.value)
+                   : std::tie(i1.port, i1.value) > std::tie(i2.port, i2.value);
+      });
+}
+
+/**
+ * @brief return the temperature list items
+ * 
+ * @return vector<Item> 
+ */
+vector<Item> Liste::items()
+{
+  return itemList;
+}
+
+/**
+ * @brief get temperature item at position
+ * 
+ * @param ix 
+ * @return Item 
+ */
+Item Liste::itemAt(int ix)
+{
+  const int size = itemList.size();
+  if (size > ix)
+    ix = size;
+  return itemList.at(ix);
+}
+
+/**
+ * @brief remove a temperature item at position
+ * 
+ * @param ix 
+ * @return true 
+ * @return false 
+ */
+bool Liste::removeAt(const int ix)
+{
+  if (itemList.size() < ix)
+    return false;
+  itemList.erase(itemList.begin() + ix);
+  return true;
+}
+
+/**
+ * @brief returns liste size
+ * 
+ * @return int 
+ */
+int Liste::getSize()
+{
+  return itemList.size();
+}
+
+/**
+ * @brief append a temperature item element at the end
+ * 
+ * @param item 
+ */
+void Liste::appendItem(Item item)
+{
+  itemList.push_back(item);
+}
+
+/**
+ * @brief set temperature item at position
+ * 
+ * @param ix 
+ * @param item 
+ * @return true 
+ * @return false 
+ */
+bool Liste::setItemAt(int ix, Item item)
+{
+  if (itemList.size() < ix)
+    return false;
+  itemList[ix] = item;
+  //itemList.insert(itemList.begin() + ix, item);
+  return true;
+}
+
+/**
+ * @brief display item at position
+ * 
+ */
+void Liste::displayAt(int ix)
+{
+  const source
+  const Item item = itemList.at(ix);
+  std::cout << "-------------------------" << std::endl;
+  std::cout << "index " << item.index << std::endl;
+  std::cout << "port " << item.port << std::endl;
+  std::cout << "timestamp " << item.timestamp << std::endl;
+  std::cout << "type " << item.type << std::endl;
+  std::cout << "value " << item.value << std::endl;
+}
+
+/**
+ * @brief display all list items
+ * 
+ */
+void Liste::displayAll()
+{
+  const int max = itemList.size();
+  for (int ix = 0; ix < max; ix++)
+  {
+    displayAt(ix);
+  }
+}
+
+/**
+ * @brief set sort Directions
+ * 
+ * @param dir 
+ */
+void Liste::setOrder(Directions dir)
+{
+  order = dir;
+}
+
+/**
+ * @brief illustrate list manipulation as min max sort
+ * g++ -g -std=c++11 -o testptr test-ptr.cpp
+ * 
+ * @return int 
+ */
+int main()
+{
+  Liste list;
+  Item item;
+  cout << "**** start generation" << endl;
+  int itemAmount = 1000000;
+  double profGen = microtime();
+  for (int i = 0; i < itemAmount; i++)
+  {
+    item.index = i;
+    item.port = i % 4; // 0..4
+    item.timestamp = "2020-01-0" + to_string(i % 30);
+    item.type = "temperature";
+    item.value = rand() % 100; // 0..100
+    list.appendItem(item);
+  }
+  cout << "++++ Profile generation : " << microtime() - profGen << "s" << endl;
+  cout << "Liste size : " << list.getSize() << endl;
+  item.index = 1000;
+  item.value = 0;
+  item.timestamp = "3020-20-20";
+  list.appendItem(item);
+  cout << "**** after append" << endl;
+  cout << "Liste size : " << list.getSize() << endl;
+  cout << "**** start sorting" << endl;
+  cout << "**** start sort index" << endl;
+  list.setOrder(Liste::DESC);
+  double profIndex = microtime();
+  list.sortByIndex();
+  list.displayAt(0);
+  cout << "++++ Profile sorted index : " << microtime() - profIndex << "s" << endl;
+  cout << "**** start sort port" << endl;
+  list.setOrder(Liste::ASC);
+  double profPort = microtime();
+  list.sortByPort();
+  list.displayAt(0);
+  cout << "++++ Profile sort port : " << microtime() - profPort << "s" << endl;
+  cout << "**** start sort value" << endl;
+  list.setOrder(Liste::DESC);
+  double profValue = microtime();
+  list.sortByValue();
+  list.displayAt(0);
+  cout << "++++ Profile sort value : " << microtime() - profValue << "s" << endl;
+  cout << "**** min index " << list.getMinIndex() << endl;
+  cout << "**** min port " << list.getMinPort() << endl;
+  cout << "**** min value " << list.getMinValue() << endl;
+  cout << "**** max index " << list.getMaxIndex() << endl;
+  cout << "**** max port " << list.getMaxPort() << endl;
+  cout << "**** max value " << list.getMaxValue() << endl;
+  //list.display();
+  return 0;
+}
